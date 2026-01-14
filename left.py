2054aac066d44d35f6384cd6bb2660f13e3d655e
August Stokes <august.stokes@learnet.se>
2026-01-14 14:29:39 +0100

    removed previous tests

--- left.py
+++ left.py
@@ -15,21 +15,7 @@ from communication import recv
 
 import time
 
-led = DigitalInOut(board.LED)
-led.direction = Direction.OUTPUT
-
-switch1 = DigitalInOut(board.GP3)
-switch1.direction = Direction.INPUT
-switch1.pull = Pull.UP
-
-switch2 = DigitalInOut(board.GP8)
-switch2.direction = Direction.INPUT
-switch2.pull = Pull.UP
-
 keyboard = Keyboard(usb_hid.devices)
-
-keys1 = AnsiKey._0
-keys2 = AnsiKey.A
 
 def read_keys() -> list[bool]:
 	pass
--- right.py
+++ right.py
@@ -12,20 +12,6 @@ from communication import send
 
 import time
 
-led = DigitalInOut(board.LED)
-led.direction = Direction.OUTPUT
-
-switch1 = DigitalInOut(board.GP3)
-switch1.direction = Direction.INPUT
-switch1.pull = Pull.UP
-
-switch2 = DigitalInOut(board.GP8)
-switch2.direction = Direction.INPUT
-switch2.pull = Pull.UP
-
-keys1 = AnsiKey._0
-keys2 = AnsiKey.A
-
 def read_keys() -> list[bool]:
 	pass
 
